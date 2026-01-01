82539a0227113e079a333d93e0ac7b084cd0a063
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2026-01-01 15:05:08 +0000

    Initial eventbridge-target module extraction
    Updated sqs dependency to use git reference

--- _variables.tf
+++ _variables.tf
@@ -1,7 +1,56 @@
-# TODO: Add your module variables here
-
+###########################################################
+#  Standard Variables
+###########################################################
 variable "tags" {
-  description = "Tags to apply to resources"
+  description = "Tags that are specific to this module"
   type        = map(string)
   default     = {}
+}
+
+###########################################################
+#  Your variables go below here
+###########################################################
+variable "name" {
+  description = "The name of the rule"
+  type        = string
+}
+
+variable "event_bus_name" {
+  description = "The name of the eventbridge instance to configure against"
+  type        = string
+}
+
+variable "event_pattern" {
+  description = "The pattern to use for matching events to the target"
+  type        = string
+  default     = null
+}
+
+variable "invocation_role_arn" {
+  description = "The role to use for all parts of the invocation"
+  type        = string
+  default     = null
+}
+
+variable "invocation_target_arn" {
+  description = "The ARN of the target for the rule"
+  type        = string
+}
+
+variable "description" {
+  description = "The description of the rule"
+  type        = string
+  default     = ""
+}
+
+variable "enable_dlq" {
+  description = "Whether to create a dead letter queue for failed events"
+  type        = bool
+  default     = true
+}
+
+variable "kms_key_arn" {
+  description = "The ARN of the KMS key used to secure the DLQ (if enabled)"
+  type        = string
+  default     = null
 }
--- main.tf
+++ main.tf
@@ -1,1 +1,42 @@
-# TODO: Add your module resources here
+resource "aws_cloudwatch_event_rule" "this" {
+  name           = var.name
+  description    = var.description
+  event_bus_name = var.event_bus_name
+  role_arn       = var.invocation_role_arn
+  event_pattern  = var.event_pattern
+
+  tags = merge(var.tags, {
+    Name = var.name
+  })
+}
+
+resource "aws_cloudwatch_event_target" "this" {
+  arn            = var.invocation_target_arn
+  rule           = aws_cloudwatch_event_rule.this.name
+  event_bus_name = var.event_bus_name
+  role_arn       = var.invocation_role_arn
+
+  dynamic "retry_policy" {
+    # When doing an eventbridge to eventbridge integration, you cannot specify a retry policy
+    for_each = length(regexall("^arn:aws:events", var.invocation_target_arn)) > 0 ? [] : [{ age = 60, attempts = 3 }]
+    content {
+      maximum_event_age_in_seconds = retry_policy.value["age"]
+      maximum_retry_attempts       = retry_policy.value["attempts"]
+    }
+  }
+
+  dynamic "dead_letter_config" {
+    for_each = var.enable_dlq ? [1] : []
+    content {
+      arn = module.dlq[0].arn
+    }
+  }
+}
+
+module "dlq" {
+  count  = var.enable_dlq ? 1 : 0
+  source = "git::https://github.com/im5tu/opentofu-aws-sqs.git?ref=main"
+
+  name        = "${var.name}-dlq"
+  kms_key_arn = var.kms_key_arn
+}
