b2016e4f000c318e5c730af9a9d2b90d03a23f5d
Stuart Blackler <Im5tu@users.noreply.github.com>
2026-01-03 15:40:54 +0000

    Initial eventbridge-target module (#1)

--- _outputs.tf
+++ _outputs.tf
@@ -1,1 +1,24 @@
-# TODO: Add your module outputs here
+output "rule_name" {
+  description = "The name of the EventBridge rule"
+  value       = aws_cloudwatch_event_rule.this.name
+}
+
+output "rule_arn" {
+  description = "The ARN of the EventBridge rule"
+  value       = aws_cloudwatch_event_rule.this.arn
+}
+
+output "target_id" {
+  description = "The ID of the EventBridge target"
+  value       = aws_cloudwatch_event_target.this.target_id
+}
+
+output "dlq_queue_arn" {
+  description = "The ARN of the dead letter queue (if created)"
+  value       = var.enable_dlq ? module.dlq[0].arn : null
+}
+
+output "dlq_queue_url" {
+  description = "The URL of the dead letter queue (if created)"
+  value       = var.enable_dlq ? module.dlq[0].url : null
+}
